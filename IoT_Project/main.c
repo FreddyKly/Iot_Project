8df2a10607fb84ea385af70201594eab541cda0f
FreddyKly <frederik.klie@gmail.com>
2022-12-20 16:01:50 +0100

    how to implement device driver

--- IoT_Project/main.c
+++ IoT_Project/main.c
@@ -2,9 +2,14 @@
 #include <msg.h>
 #include "example_driver.c"
 
+extern saul_driver_t adc_saul_driver;
 
 int main(void){
-    saul_reg_add(a pointer to idk what (driver i guess));
+    saul_reg_t reg;
+    reg.name = "name";
+    reg.dev = (void *) 1;
+    reg.driver = &adc_saul_driver;
+    saul_reg_add(reg);
     msg_t msg_q[8];
     msg_init_queue(msg_q, 8);
     static char buf[SHELL_DEFAULT_BUFSIZE];
