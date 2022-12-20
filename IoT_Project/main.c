792bb801e46586c594f61a21c50624507e06cc00
FreddyKly <frederik.klie@gmail.com>
2022-12-20 15:44:31 +0100

    useful additions

--- IoT_Project/main.c
+++ IoT_Project/main.c
@@ -1,12 +1,15 @@
 #include <shell.h>
+#include <msg.h>
+#include "example_driver.c"
 
 
 int main(void){
+    saul_reg_add(a pointer to idk what (driver i guess));
     msg_t msg_q[8];
     msg_init_queue(msg_q, 8);
     static char buf[SHELL_DEFAULT_BUFSIZE];
     
-    shell_run_forever(null, buf, sizeof(buf)); //RIOT method -> documentation riot-os.org
+    shell_run_forever(NULL, buf, sizeof(buf)); //RIOT method -> documentation riot-os.org
     
     return 0;
 }
