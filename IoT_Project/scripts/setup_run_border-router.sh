dc9a13299a36064ea08303a2b1eb331d0cc8cd30
FreddyKly <Frederik.Kliemt@web.de>
2023-01-13 16:40:57 +0100

    Script creates a Border-router

--- IoT_Project/scripts/setup_run_border-router.sh
+++ IoT_Project/scripts/setup_run_border-router.sh
@@ -19,13 +19,10 @@ scp ./utils/gnrc_border_router.elf $login@grenoble.iot-lab.info:./A8
 echo "Files were copied to IoT-Lab grenoble server ./A8"
 
 ssh -t $login@grenoble.iot-lab.info ssh -t root@node-a8-$node /bin/bash << 'EOF'
-all_ipv6=$(ip addr show dev eth0 | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d');
-ip_adress=$(echo $all_ipv6| cut -d' ' -f 1);
-echo $ip_adress;
 cd A8;
 iotlab_flash gnrc_border_router.elf;
 cd ~/A8/riot/RIOT/dist/tools/uhcpd;
 make clean all;
 cd ../ethos; make clean all;
-./start_network.sh /dev/ttyA8_M3 tap0 2001:660:3207:401::/64 500000
+./start_network.sh /dev/ttyA8_M3 tap0 $INET6_PREFIX::/64 500000
 EOF
